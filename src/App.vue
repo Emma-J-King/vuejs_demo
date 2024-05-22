09c936f7179f95aac1d0c6f2131fe62cc6d960d2
emma j king <emmajking@live.co.uk>
2024-05-22 18:29:47 +0100

    slideshow

--- src/App.vue
+++ src/App.vue
@@ -15,26 +15,76 @@ export default defineComponent({
     const joke = ref("three");
     //create onclick image array
     // replace images within the array
-    const imgLoc = ref([
-      "https://images.pexels.com/photos/1670413/pexels-photo-1670413.jpeg?cs=srgb&dl=pexels-prasanthdas-1670413.jpg&fm=jpg",
-      "https://t4.ftcdn.net/jpg/01/66/10/03/360_F_166100342_KbTGIRrnrlwGDZSXSMpH3zfn2dxyTKae.jpg",
-      "https://cdn.pixabay.com/photo/2024/04/18/19/14/monkey-8704855_1280.jpg",
-      "https://images.rawpixel.com/image_png_social_square/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA3L3JvYl9yYXdwaXhlbF9waG90b2dyYXBoX29mX2FfYmFieV9vcmFndXRhbl9pc29sYXRlZF9vbl93aGl0ZV9zdF84ZTE2OTRmZi1lOWU2LTQ0MTEtYWQzNC00NTdjOGE4NjBhNzYtNXgtaHEtc2NhbGUtNV8wMHgtam9iMTg5NF8xLnBuZw.png",
-      "https://images.freeimages.com/images/large-previews/497/uluwatu-monkey-1376207.jpg?fmt=webp&w=500",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWBDue7ImdDCVK1_wHPP1U2Tnmy6HXn5XOURixHNabKg&s"
-    ]);
+    const images = ref([
+      {
+        "loc": "https://images.unsplash.com/photo-1551884831-bbf3cdc6469e?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "photographerCredit": "Magdalena Smolnicka"
+      },
 
-    const index = ref(0);
-    const imageChange = function () {
+      {
+        "loc": "https://media.istockphoto.com/id/1266736164/photo/playful-pup-on-the-bed.jpg?s=2048x2048&w=is&k=20&c=Re6-pVxU4UEjYiS5255-df1sQfmJrCBX-u6x0DArCEA=",
+        "photographerCredit": "SolStock"
+      },
+
+      {
+        "loc": "https://media.npr.org/assets/img/2022/12/09/duck-92ad2f54daa7edb182ba94daa1e6e7cfa04a070c.jpg?s=2600&c=85&f=webp",
+        "photographerCredit": "Ryan Sims"
+      },
+
+      {
+        "loc": "https://static.euronews.com/articles/stories/04/97/98/10/828x1242_cmsv2_792920c6-ff80-54db-b713-54fbbf7a1062-4979810.jpg",
+        "photographerCredit": "Brigitte Alcalay Marcon"
+      },
+
+      {
+        "loc":"https://hips.hearstapps.com/hmg-prod/images/lionel-animals-to-follow-on-instagram-1568319926.jpg?",
+        "photographerCredit": "Picture courtesy of Good Housekeeping"
+      },
+    ]);
+    
+
+     const index = ref(0);
+     const imageLeft = function () {
       // increment index
       index.value ++
       // catch too high a number
-      if (index.value === imgLoc.value.length){
+      if (index.value === images.value.length){
         index.value = 0
       }
     };
+   
+
+    const images2 =ref([
+      {
+        "loc":"https://media.istockphoto.com/id/1402979585/photo/goat-with-his-tongue-out.jpg?s=612x612&w=0&k=20&c=9pE4Mlx9f5245CWJGtaodpG3JGN-c1iYA4JDQRCBxg0=",
+        "photographerCredit": "John Carnemolla"
+      },
+
+      {
+        "loc":"https://media.npr.org/assets/img/2022/12/09/squirreljump-28aafd8cd34db8db8604eb503509496394234554.jpg?",
+        "photographerCredit":"Alex Pansier"
+      }
+    ]);
+    const index2 =ref(0);
+    const imagechangeRight = function () {
+      // Decrement index
+      index2.value--;
+      // Catch too low a number
+      if (index2.value < 0){
+        index2.value = images2.value.length - 1;
+      }
+    };
+
+      const startSlideshow = () => {
+        const interval = setInterval(imagechangeRight, 5000); // Change image every 5 seconds
+
+      
+      };
+
+     
     
     onMounted(() => {
+      startSlideshow();
       axios.get(
         'https://icanhazdadjoke.com/', { //arguments need to be applied 
           responseType: 'json',
@@ -54,10 +104,15 @@ export default defineComponent({
     return {
       test,
       joke,
-      // add onclick (image location, imagechange and index)
-      imgLoc,
-      imageChange,
-      index
+      // add onclick (image location, imagechange and index
+      // imgLoc,
+      imageLeft,
+      imagechangeRight,
+      index,
+      index2,
+      images,
+      images2
+      
     }
   },
 });
@@ -68,14 +123,15 @@ export default defineComponent({
 
 </script> -->
 <template>
-  <header>
-    Workplace breather
+  <header style="background-color: rgb(47, 47, 170);">
+   <p style="text-align: center;"><span style="color: white; font-size: 40px;">Workplace breather</span></p>
+    <br>
+  <br>
+  
   </header>
 
 <body>
-  <br>
-  <br>
-  <br>
+  
   <div class="container cc">
       <div class="row c">
         <HelloWorld msg="Do you like jokes?" />
@@ -90,21 +146,25 @@ export default defineComponent({
       <br>
       <p> Click for more funny animals</p> 
       <img 
-      @click="imageChange" 
+      @click="imageLeft" 
       height="300px" width="300px" 
-      :src="imgLoc[index]"
-    >
-
+      :src="images[index].loc">
+      <br>
+      Picture credit goes to:
+      <p>{{images[index].photographerCredit }}</p>
      
 
     </div>
     <div class="row image">
       <br>
       <p> Or we can click for you here</p>
-      <img src="https://hips.hearstapps.com/hmg-prod/images/lionel-animals-to-follow-on-instagram-1568319926.jpg?" height="300px" width="300px">
+      <img
+      @startSlideshow="imagechangeRight" 
+      height="300px" width="300px"
+      :src="images2[index2].loc" >
       <br>
-      Picture courtesy of Good Housekeeping
-    
+      Picture credit goes to:
+      <p>{{images2[index2].photographerCredit }}</p>
     </div>
   </div>
   
@@ -114,6 +174,13 @@ export default defineComponent({
 </template>
 
 <style>
+
+.header {
+  border-style: dotted;
+  border-color: brown;
+background-color: rgb(47, 47, 170);
+text-align: center;
+}
 
 .c{
   border-style: solid;
@@ -153,5 +220,9 @@ margin: 150px;
   height:300px;
 
 } 
+.span {
+  color: white; font-size: 40px; text-align: center;
+}
+
 </style>
 
